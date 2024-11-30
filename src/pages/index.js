ce2307af848a7740179aa94c5f787542082e11ce
pavel fomichev <pfomicev68@gmail.com>
2024-12-01 00:16:24 +0300

    подкорректироовал название метода

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,89 +1,95 @@
-import Card from '../scripts/components/Card.js';
-import PopupWithFormAddCard from '../scripts/components/PopupWithFormAddCard.js';
-import PopupWithFormSignIn from '../scripts/components/PopupWithFormSignIn.js';
-import PopupWithFormSignUp from '../scripts/components/PopupWithFormSignUp.js';
-import { exampleCardsInfo, buttonsAddCard, buttonsRegistry, buttonsSign, cardsList, freshFooterCoordinateValue } from '../scripts/utils/constants.js';
-import PopupWithImage from '../scripts/components/PopupWithImage.js';
-import Scroll from '../scripts/components/Scroll.js';
-import DeleteCards from '../scripts/components/DeleteCards.js';
+import Card from "../scripts/components/Card.js";
+import PopupWithFormAddCard from "../scripts/components/PopupWithFormAddCard.js";
+import PopupWithFormSignIn from "../scripts/components/PopupWithFormSignIn.js";
+import PopupWithFormSignUp from "../scripts/components/PopupWithFormSignUp.js";
+import {
+  exampleCardsInfo,
+  buttonsAddCard,
+  buttonsRegistry,
+  buttonsSign,
+  cardsList,
+  freshFooterCoordinateValue,
+} from "../scripts/utils/constants.js";
+import PopupWithImage from "../scripts/components/PopupWithImage.js";
+import Scroll from "../scripts/components/Scroll.js";
+import DeleteCards from "../scripts/components/DeleteCards.js";
 
-import './index.css';
+import "./index.css";
 
-buttonsAddCard.forEach( (item) => {
-    item.addEventListener('click', function() {
-        popupWithFormAddCard.openPopup();
-    });
+buttonsAddCard.forEach((item) => {
+  item.addEventListener("click", function () {
+    popupWithFormAddCard.openPopup();
+  });
 });
 
 buttonsSign.forEach((item) => {
-    item.addEventListener('click', function() {
-        popupWithFormSignIn.openPopup();
-    });
+  item.addEventListener("click", function () {
+    popupWithFormSignIn.openPopup();
+  });
 });
 
 buttonsRegistry.forEach((item) => {
-    item.addEventListener('click', function () {
-        popupWithFormSignUp.openPopup();
-    });
+  item.addEventListener("click", function () {
+    popupWithFormSignUp.openPopup();
+  });
 });
 
 function createCard(objectItem) {
+  const cardObject = new Card({
+    objectItem,
+    templateSelector: ".card-template",
+    getCardImageSrc(imageSrcValue) {
+      popupWithImage.setPopupImageSrc(imageSrcValue);
+      popupWithImage.openPopup();
+    },
+  });
 
-    const cardObject = new Card({
-        objectItem,
-        templateSelector: '.card-template',
-        getCardImageSrc(imageSrcValue) {
-            popupWithImage.changePopupImageSrc(imageSrcValue);
-            popupWithImage.openPopup();
-        }
-    });
+  const cardElement = cardObject.addCard();
 
-    const cardElement = cardObject.addCard();
+  cardsList.prepend(cardElement);
 
-    cardsList.prepend(cardElement);
-
-    freshFooterCoordinateValue();
+  freshFooterCoordinateValue();
 }
 
-exampleCardsInfo.forEach( item => {
-    createCard(item);
+exampleCardsInfo.forEach((item) => {
+  createCard(item);
 });
 
 const popupWithFormAddCard = new PopupWithFormAddCard({
-    popupSelector: '.popup_add-form',
-    submit(dataObj) {
-        createCard(dataObj);
-        popupWithFormAddCard.closePopup();
-        deleteCards.newCardForDelete();
-    }
+  popupSelector: ".popup_add-form",
+  submit(dataObj) {
+    createCard(dataObj);
+    popupWithFormAddCard.closePopup();
+    deleteCards.newCardForDelete();
+  },
 });
 
 popupWithFormAddCard.setEventListeners();
 
 const popupWithFormSignIn = new PopupWithFormSignIn({
-    popupSelector: '.popup_sign-form',
-    submit(dataObj) {
-        popupWithFormSignIn.closePopup();
-        alert('а вот и нет профиля');
-        dataObj = {};
-    }
+  popupSelector: ".popup_sign-form",
+  submit(dataObj) {
+    popupWithFormSignIn.closePopup();
+    alert("а вот и нет профиля");
+    dataObj = {};
+  },
 });
 
 popupWithFormSignIn.setEventListeners();
 
 const popupWithFormSignUp = new PopupWithFormSignUp({
-    popupSelector: '.popup_registry-form',
-    submit(dataObj) {
-        popupWithFormSignUp.closePopup();
-        alert('а вот и нет профиля');
-        dataObj = {};
-    }
+  popupSelector: ".popup_registry-form",
+  submit(dataObj) {
+    popupWithFormSignUp.closePopup();
+    alert("а вот и нет профиля");
+    dataObj = {};
+  },
 });
 
 popupWithFormSignUp.setEventListeners();
 
 const popupWithImage = new PopupWithImage({
-    popupSelector: '.popup_card-image'
+  popupSelector: ".popup_card-image",
 });
 
 popupWithImage.setEventListeners();
@@ -91,7 +97,10 @@ popupWithImage.setEventListeners();
 const scrollY = new Scroll();
 scrollY.scrollWork();
 
-const deleteCards = new DeleteCards(scrollY.toggleFixedBlockVisible, scrollY.contentTopCoordinates);
+const deleteCards = new DeleteCards(
+  scrollY.toggleFixedBlockVisible,
+  scrollY.contentTopCoordinates
+);
 
 deleteCards.setEventListeners();
 
--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -1,21 +1,23 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-    constructor({popupSelector}) {
-        super({popupSelector});
-        this._popupElementContainer = this._popupElement.querySelector('.popup__image-container');
-        this._popupElementImage = this._popupElementContainer.querySelector('.popup__image');
+  constructor({ popupSelector }) {
+    super({ popupSelector });
+    this._popupElementContainer = this._popupElement.querySelector(
+      ".popup__image-container"
+    );
+    this._popupElementImage =
+      this._popupElementContainer.querySelector(".popup__image");
+  }
 
-    }
+  setPopupImageSrc(image) {
+    this._popupElementImage.src = image;
+  }
 
-    changePopupImageSrc(image) {
-        this._popupElementImage.src = image;
-    }
-
-    closePopup() {
-        super.closePopup();
-        setTimeout(() => {
-            this._popupElementImage.src = "";
-        }, 500);
-    }
+  closePopup() {
+    super.closePopup();
+    setTimeout(() => {
+      this._popupElementImage.src = "";
+    }, 500);
+  }
 }
